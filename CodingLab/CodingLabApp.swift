54e24c3f7d11786ae01db76a5d9163e633ff341e
Furkan COSGUN <furkan51cosgun@gmail.com>
2023-01-26 22:09:26 +0300

    firstCommit

---
+++ CodingLab/CodingLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingLabApp.swift
+//  CodingLab
+//
+//  Created by Furkan on 25.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CodingLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
